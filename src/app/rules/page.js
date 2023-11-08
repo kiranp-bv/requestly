c669fa94573004ba8e5375f048742af7d12d0bcb
Kiran P <kiran.p@bazaarvoice.com>
2023-11-08 21:00:53 +0530

    Added landing page UI

--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -1,7 +1,50 @@
+'use client';
+import Switch from '../components/Switch'
+import './index.css'
+import 'material-icons/iconfont/material-icons.css'
+import { useState } from 'react'
 export default () => {
+    const [blockEdit, setBlockEdit] = useState(true) 
+    const [redirectEdit, setRedirectEdit] = useState(false) 
+
+    const handleEditHover = (type, hover) => {
+        switch(type) {
+            case 'block':
+                setBlockEdit(hover)
+                break;
+            case 'redirect':
+                setRedirectEdit(hover)
+                break;
+        }
+    }
     return (
-        <div className='main-section'>
-            Landing Page
+        <div>
+            <div className='main-section'>
+                <div className='switch-section'>
+                    <Switch/>
+                </div>
+                <div className='rules-section'>
+                    <div className='redirect rule-section' onMouseEnter={() => handleEditHover('redirect', true)} onMouseLeave={() => handleEditHover('redirect', false)}>
+                        <span className='rule-label'>
+                            Redirect
+                        </span>
+                        {
+                            redirectEdit &&
+                            <span className="material-icons edit-icon">edit</span>
+
+                        }
+                    </div>
+                    <div className='block rule-section' onMouseEnter={() => handleEditHover('block', true)} onMouseLeave={() => handleEditHover('block', false)}>
+                        <span className='rule-label'>
+                            Block
+                        </span>
+                        {
+                            blockEdit &&
+                            <span className="material-icons edit-icon">edit</span>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
